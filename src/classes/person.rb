e08e2455038d0c3f27ffcd6c178ca806b876f6e1
belaymit <2belamit@gmail.com>
2023-03-07 15:38:49 +0300

    inherited nameable class

--- src/classes/person.rb
+++ src/classes/person.rb
@@ -1,19 +1,31 @@
-class Person
+require_relative '../decorators/nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, parent_permission: true, name: 'Unknown')
+  def initialize(age,  name, parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def of_age?
-    @age >= 18
+  def correct_name
+    @name
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+p person.correct_name
