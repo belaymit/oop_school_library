160a6dd5f558bdbce7738fd1e5ae8836997eee56
belaymit <2belamit@gmail.com>
2023-03-07 23:12:58 +0300

    made the required change

--- src/test.rb
+++ src/test.rb
@@ -6,5 +6,5 @@ person = Person.new(22, 'maximilianus')
 p person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 p capitalized_person.correct_name
-trimmed_person = TrimmerDecorator.new(capitalizedPerson)
+trimmed_person = TrimmerDecorator.new(capitalized_person)
 p trimmed_person.correct_name
