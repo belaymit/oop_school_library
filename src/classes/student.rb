05bc7da53d86c17f26de829c2c566ba983bc992a
belaymit <2belamit@gmail.com>
2023-03-11 11:44:43 +0300

    modified student constructor

--- src/classes/student.rb
+++ src/classes/student.rb
@@ -3,7 +3,7 @@ require_relative './person'
 class Student < Person
   attr_reader :classroom, :type
 
-  def initialize(classroom, age, name: 'Unknown', parent_permission: true, type: 'Student')
+  def initialize(classroom, age, name, parent_permission: true, type: 'Student')
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
     @type = type
