6c68ccdb67dfb68c50dc9b18895730d70dc44133
belaymit <2belamit@gmail.com>
2023-03-13 21:33:49 +0300

    reafactored app method

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ require_relative './src/classes/rental'
 require_relative './src/modules/user_choice'
 require_relative './src/modules/list'
 require_relative './src/modules/create_people'
+require_relative './src/modules/create_book'
+require_relative './src/modules/create_rental'
 
 class App
   attr_reader :books, :people, :rentals
@@ -24,45 +26,12 @@ class App
 
   include CreatePeople
 
-  def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    book = Book.new(title, author)
-    @books.push(book)
-    puts @books.length
-    puts 'Book has been created successfully'
-  end
+  include CreateBook
 
-  def create_rental
-    puts 'Select a book by number'
-    @books.each_with_index { |book, i| puts(" #{i}) | Title: #{book.title} Author: #{book.author} |") }
-    index = gets.chomp.to_i
-    book = @books[index]
+  include CreateRental
 
-    puts 'Select a person by number (not ID)'
-    @people.each_with_index do |person, i|
-      puts("  #{i}) [#{person.class}] ID: #{person.id} Name: #{person.name} Age: #{person.age}")
-    end
-    index = gets.chomp.to_i
-    person = @people[index]
-
-    date = Time.now.strftime('%m/%d/%y')
-    rental = Rental.new(date, book, person)
-    @rentals.push(rental)
-    person.add_rental(rental)
-    book.add_rental(rental)
-    puts "Date:  #{date}"
-    puts('Book has been rented successfully')
-  end
-
-  def load_rentals
-    print 'Person ID '
-    person_id = gets.chomp.to_i
-    person = @people.select { |per| per.id == person_id }
-    puts 'No Person found' unless person.length.positive?
-    puts 'Rentals: '
-    rentals.each { |rent| puts "Date: #{rent.date}, Book #{rent.book.title} by #{rent.person.name}" }
+  def quit_app
+    puts 'Thank you for using this App!'
+    exit
   end
 end
