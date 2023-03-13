b370dd0dc8696f035cbae6b4178c6498cd3b4327
belaymit <2belamit@gmail.com>
2023-03-13 20:06:37 +0300

    refactored main and app

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require_relative './src/classes/student'
 require_relative './src/classes/teacher'
 require_relative './src/classes/book'
 require_relative './src/classes/rental'
+require_relative './src/modules/user_choice'
 
 class App
   attr_reader :books, :people, :rentals
@@ -14,39 +15,9 @@ class App
   end
 
   def run
-    prompt_user
+    user_choice.main
   end
 
-  def loads_books
-    puts "\nNo books in the Library\n\n" unless @books.length.positive?
-    @books.each { |book| puts("Title: #{book.title}, Author: #{book.author}\n") }
-  end
-
-  def load_people
-    puts "\nNo People found\n\n" unless @people.length.positive?
-    @people.each { |person| puts("  [#{person.class}] ID: #{person.id} Name: #{person.name} Age: #{person.age}") }
-  end
-
-  def create_person
-    # code to create a person (teacher or student)
-    puts 'Create a person'
-    print 'Do you want to create a Student (1) or a Teacher (2)?[Input a Number]: '
-    person_type = gets.chomp.to_i
-    if [1, 2].include? person_type
-      print 'Name: '
-      name = gets.chomp
-      print 'Age: '
-      age = gets.chomp.to_i
-      case person_type
-      when 1
-        create_student(age, name)
-      when 2
-        create_teacher(age, name)
-      end
-    else
-      puts 'Invalid'
-    end
-  end
 
   def create_student(age, name)
     print 'Has parent permission? [Y/N]: '
