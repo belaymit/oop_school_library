a5dc065c1eb78685617155b7b7162232c7cabd06
belaymit <2belamit@gmail.com>
2023-03-14 19:39:12 +0300

    created initial methods to persist data

--- app.rb
+++ app.rb
@@ -8,14 +8,28 @@ require_relative './src/modules/list'
 require_relative './src/modules/create_people'
 require_relative './src/modules/create_book'
 require_relative './src/modules/create_rental'
+require 'json'
 
 class App
   attr_reader :books, :people, :rentals
 
+  def get_books
+    return [] unless File.exist?('books.json')
+    book_items = File.read('books.json')
+    book_list = JSON.parse(book_items)
+    book_list.each{Book.new('title', 'author')}
+    # return []
+  end
+  def get_people
+  end
+
+  def get_rentals
+  end
+
   def initialize
-    @people = []
-    @books = []
-    @rentals = []
+    @people = get_people
+    @books =  get_books
+    @rentals = get_rentals
   end
 
   include List
