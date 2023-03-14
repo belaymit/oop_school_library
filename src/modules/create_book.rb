5377cb2befdfe8d94cb8617203053663f1789b34
belaymit <2belamit@gmail.com>
2023-03-14 19:39:35 +0300

    implemented a method to persist books

--- src/modules/create_book.rb
+++ src/modules/create_book.rb
@@ -1,13 +1,25 @@
+require 'json'
+
 module CreateBook
+  def persit_books(title, author)
+    book_item = {'title'=>title, 'author'=>author}
+    # file = File.open('books.json')
+    book = File.read('books.json')
+    @books = JSON.parse(book)
+    @books << book_item 
+    File.write('books.json', JSON.pretty_generate(@books))
+  end
+
   def create_book
     print 'Title: '
     title = gets.chomp
     print 'Author: '
     author = gets.chomp
     book = Book.new(title, author)
-    @books.push(book)
-    # @books << book unless @books.include?(book)
-    puts @books.length
+    # @books.push(book)
+    @books << book unless @books.include?(book)
+    # puts @books.length
+    persit_books(title, author)
     puts 'Book has been created successfully'
   end
 end
