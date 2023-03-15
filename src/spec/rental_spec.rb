c55aac47c2fab7af0f05d8ca5f407714f9ff8491
belaymit <2belamit@gmail.com>
2023-03-15 20:13:39 +0300

    added test_case for rental

--- src/spec/rental_spec.rb
+++ src/spec/rental_spec.rb
@@ -1,1 +1,17 @@
 require_relative './spec_helper'
+describe Rental do
+  new_rental = Rental.new('2023-03-14', 'Mata', 'Naked Triangle')
+
+  it 'Shoould be an instance of Rental' do
+    expect(new_rental).to be_an_instance_of(Rental)
+  end
+  it 'The date when the book is rented should be "2023-03-14"' do
+    expect(new_rental.date).to eq('2023-03-14')
+  end
+  it 'The name of a person who rented the book should be "Mata"' do
+    expect(new_rental.person).to eq('Mata')
+  end
+  it 'The title of the book should be "Naked Triangle"' do
+    expect(new_rental.book).to eq('Naked Triangle')
+  end
+end
