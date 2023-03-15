8be9cd7b5a05134009e44a5139137b2c09fe60de
belaymit <2belamit@gmail.com>
2023-03-15 20:36:45 +0300

    added test case for teachers

--- src/spec/teacher_spec.rb
+++ src/spec/teacher_spec.rb
@@ -1,1 +1,25 @@
 require_relative './spec_helper'
+
+describe Teacher do
+  new_teacher = Teacher.new('Peter', 32, 'Physics')
+
+  it 'Should be an instance of Teacher class' do
+    expect(new_teacher).to be_an_instance_of(Teacher)
+  end
+
+  it 'Should return true if a Teacher can use service' do
+    expect(new_teacher.can_use_services?).to be true
+  end
+
+  it 'The specialization should be "Physics"' do
+    expect(new_teacher.specialization).to eq('Physics')
+  end
+
+  it 'The name of the teacher should be Peter' do
+    expect(new_teacher.name).to eq('Peter')
+  end
+
+  it 'Should have renatls instance' do
+    expect(new_teacher.rentals).to be_an_instance_of(Array)
+  end
+end
